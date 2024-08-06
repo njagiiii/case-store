8a53d95c8e4b4d3117ed27dc1af5af91013a60e6
Grace <keshgrace62@gmail.com>
2024-08-06 17:28:29 +0300

    thankyou page

--- src/app/thank-you/Thankyou.tsx
+++ src/app/thank-you/Thankyou.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { getPaymentStatus } from "./actions";
 import { useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import PhonePreview from "@/components/PhonePreview";
 
 const Thankyou = () => {
   const searchParams = useSearchParams();
@@ -83,6 +84,7 @@ const Thankyou = () => {
           </div>
 
           <div className="flex space-x-6 overflow-hidden mt-4 rounded-xl bg-gray-900/5 reng-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
+          <PhonePreview  croppedImageUrl={configuration.croppedImageUrl!} color={color!}/>
            
           </div>
 
