516805535be370cb6a4d473547e8db6739c157d3
Grace <keshgrace62@gmail.com>
2024-08-06 21:06:21 +0300

    thankyou page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { notFound } from 'next/navigation'
 import { db } from '@/db'
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { formatPrice } from '@/lib/utils'
 
 
@@ -48,6 +48,8 @@ const DashboardPage = async () => {
         }
 
     })
+
+    const WEEKLY_GOAL = 65000
   return <div className="flex min-h-screen w-full bg-muted/40">
     <div className="max-w-7xl w-full mx-auto flex flex-col sm:gap-4 sm:py-4">
         <div className="flex flex-col gap-16">
@@ -57,6 +59,11 @@ const DashboardPage = async () => {
                         <CardDescription>Last Week</CardDescription>
                         <CardTitle className='text-4xl'>{formatPrice(lastWeekSum._sum.amount ?? 0)}</CardTitle>
                     </CardHeader>
+                    <CardContent>
+                        <div className="text-sm text-muted-foreground">
+                            of {formatPrice(WEEKLY_GOAL)}
+                        </div>
+                    </CardContent>
                 </Card>
             </div>
         </div>
