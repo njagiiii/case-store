a9b23eb2ca304f063af5f7ab352c7283b2788ac6
Grace <keshgrace62@gmail.com>
2024-08-06 17:26:14 +0300

    thankyou page

--- src/components/PhonePreview.tsx
+++ src/components/PhonePreview.tsx
@@ -1,11 +1,53 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { CaseColor } from "@prisma/client";
+import React, { useRef, useState } from "react";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { cn } from "@/lib/utils";
 
-const PhonePreview = () => {
+const PhonePreview = ({
+  croppedImageUrl,
+  color,
+}: {
+  croppedImageUrl: string;
+  color: CaseColor;
+}) => {
+  // state to manage image width depending on different screens
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [renderedDimensions, setRenderedDimensions] = useState({
+    height: 0,
+    width: 0,
+  });
+
+  let caseBackgroundColor = 'bg-zinc-950'
+  if(color === "blue") caseBackgroundColor = "bg-blue-950"
+   if(color === "rose") caseBackgroundColor = "bg-rose-950"
+    if(color === "yellow") caseBackgroundColor = "bg-yellow-950"
   return (
-    <div>PhonePrevies</div>
-  )
-}
+    <AspectRatio ref={ref} ratio={3000 / 2001} className="relative">
+      <div
+        className="absolute z-20 scale-[1.0352]"
+        style={{
+          left:
+            renderedDimensions.width / 2 -
+            renderedDimensions.width / (1216 / 121),
+          top: renderedDimensions.height / 6.22,
+        }}
+      >
+        <img
+          width={renderedDimensions.width / (3000 / 637)}
+          className={cn("phone-skew relative z-20 rounded-t-[15px] rounded=b=[10px] md:rounded-t-[30px] md:rounded-b-[20px]")}
+          src={croppedImageUrl}
+        />
+      </div>
 
-export default PhonePreview
+      <div className="relative h-full w-full z-40">
+        <img alt="phone" src='/clearphone.png' className="pointer-events-none h-full w-full antialiased rounded-md"/>
+      </div>
+    </AspectRatio>
+  );
+};
+
+export default PhonePreview;
