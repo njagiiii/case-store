9cab36dc19d96438b0a6b52c7435d7d235ada9bb
Grace <keshgrace62@gmail.com>
2024-08-06 21:17:29 +0300

    thankyou page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { notFound } from 'next/navigation'
 import { db } from '@/db'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { formatPrice } from '@/lib/utils'
+import { Progress } from '@/components/ui/progress'
 
 
 const DashboardPage = async () => {
@@ -48,8 +49,21 @@ const DashboardPage = async () => {
         }
 
     })
+// last month sum
+    const lastMonthSum = await db.order.aggregate({
+        where: {
+            isPaid: true,
+            createdAt: {
+                gte: new Date(new Date(). setDate(new Date().getDate() - 30))
+            }
+        },
+        _sum : {
+            amount: true
+        }
 
+    })
     const WEEKLY_GOAL = 65000
+    const MONTHLY_GOAL = 32000
   return <div className="flex min-h-screen w-full bg-muted/40">
     <div className="max-w-7xl w-full mx-auto flex flex-col sm:gap-4 sm:py-4">
         <div className="flex flex-col gap-16">
@@ -64,6 +78,24 @@ const DashboardPage = async () => {
                             of {formatPrice(WEEKLY_GOAL)}
                         </div>
                     </CardContent>
+                    <CardFooter>
+                        <Progress value={((lastWeekSum._sum.amount ?? 0) / WEEKLY_GOAL) * 100 }/>
+                    </CardFooter>
+                </Card>
+                {/* last month */}
+                <Card>
+                    <CardHeader className='pb-2'>
+                        <CardDescription>Last Month</CardDescription>
+                        <CardTitle className='text-4xl'>{formatPrice(lastMonthSum._sum.amount ?? 0)}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-sm text-muted-foreground">
+                            of {formatPrice(MONTHLY_GOAL)}
+                        </div>
+                    </CardContent>
+                    <CardFooter>
+                        <Progress value={((lastMonthSum._sum.amount ?? 0) / MONTHLY_GOAL) * 100 }/>
+                    </CardFooter>
                 </Card>
             </div>
         </div>
